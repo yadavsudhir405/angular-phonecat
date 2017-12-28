74a6b6116479d237793a2f03a2bfb4357af19e87
Sudhir Yadav <yadavsudhir405@gmail.com>
2017-12-28 12:38:49 +0530

    Enabled router and config

--- app/res.js
+++ app/res.js
@@ -1,33 +1,19 @@
 'use strict';
 
-var demoApp = angular.module('demoApp', []);
 
-demoApp.config(function ($routeProvider) {
-    console.log('Inside config');
-    $routeProvider
-        .when('/',
-            {
-                controller: 'SimpleController',
-                templateUrl: 'partials/view1.html'
-            })
-        .when('/view2',
-            {
-                controller: 'SimpleController',
-                templateUrl: 'partials/view2.html'
-            })
-        .otherwise({redirectTo: '/'});
-});
-demoApp.controller('SimpleController', function ($scope) {
-    $scope.customers = [
-        {name:'John Smith',city:'Bangalore'},
-        {name:'John Doe',city:'Delhi'},
-        {name:'Frank',city:'Chennai'}
-    ];
-    $scope.addCustomer = function () {
-        $scope.customers.push(
-            {
-                name: $scope.newCustomer.name, city: $scope.newCustomer.city
-            }
-        );
+angular.
+module('phonecatApp').
+config(['$locationProvider' ,'$routeProvider',
+    function config($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
+
+        $routeProvider.
+        when('/phones', {
+            template: '<phone-list></phone-list>'
+        }).
+        when('/phones/:phoneId', {
+            template: '<phone-detail></phone-detail>'
+        }).
+        otherwise('/phones');
     }
-});
+]);
